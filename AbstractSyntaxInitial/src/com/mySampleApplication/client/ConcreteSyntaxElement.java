6bf1e37cbb2e5814e2ce97b4d1eca2358323697d
MichaelJCompton <username@users.noreply.github.com>
2015-08-19 23:22:42 +1000

    V2

--- AbstractSyntaxInitial/src/com/mySampleApplication/client/ConcreteSyntaxElement.java
+++ AbstractSyntaxInitial/src/com/mySampleApplication/client/ConcreteSyntaxElement.java
@@ -3,6 +3,8 @@ package com.mySampleApplication.client;
 import com.ait.lienzo.client.core.shape.Layer;
 import com.ait.lienzo.client.core.shape.Shape;
 import com.ait.lienzo.client.core.shape.Text;
+import com.ait.lienzo.client.core.types.Point2D;
+import com.ait.lienzo.shared.core.types.ColorName;
 import michael.com.AbstractDiagram;
 import michael.com.DiagramElement;
 
@@ -15,41 +17,129 @@ import com.ait.lienzo.shared.core.types.Color;
  */
 public abstract class ConcreteSyntaxElement {
 
+    // FIXME : use point2d for (x,y)
+
+
+    protected enum ConcreteSyntaxElement_TYPES {
+        CONCRETEARROW, CONCRETEBOUNDARYRECTANGE, CONCRETECURVE, CONCRETESPIDER, CONCRETEZONE
+    }
+    private ConcreteSyntaxElement_TYPES myType;
+
+
+    protected static final Color visibleWhite = new Color(255,255,255,1);     // 1 = invisible
+    protected static final Color invisibleWhite = new Color(255,255,255,0);     // 0 = invisible
+
+
+    protected static final ColorName curveBorderColourName = ColorName.BLACK;
+    protected static final Color curveBorderColour = curveBorderColourName.getColor();
+    protected static final Color curveFillColour = invisibleWhite;
+    protected static final ColorName curveBorderSelectedColorName = ColorName.RED;
+    protected static final Color curveBorderSelectedColor = curveBorderSelectedColorName.getColor();
+
+    protected static final double curveCornerRadius = 10;
+    protected static final double curveBorderWidth = 2;
+    protected static final double curveMinWidth = (curveCornerRadius * 4);  // no small curves constraint
+    protected static final double curveMinHeight = curveMinWidth;           // no small curves constraint
+
+
+    protected static final ColorName spiderColourName = ColorName.BLACK;
+    protected static final Color spiderColour = spiderColourName.getColor();
+    protected static final ColorName spiderSelectedColorName = ColorName.RED;
+    protected static final Color spiderSelectedColor = spiderSelectedColorName.getColor();
+
+    protected static final double spiderRadius = 7;
+
+
+    protected static final ColorName boundaryRectangleColourName = ColorName.BLACK;
+    protected static final Color boundaryRectangleColour = boundaryRectangleColourName.getColor();
+    protected static final Color boundaryRectangleFillColour = invisibleWhite;
+    protected static final ColorName boundaryRectangleSelectedColourName = ColorName.RED;
+    protected static final Color boundaryRectangleSelectedColour = boundaryRectangleSelectedColourName.getColor();
+
+    protected static final double boundaryRectangleBorderWidth = curveBorderWidth * 2;
+    protected static final double boundaryRectangleMinWidth = (curveMinWidth * 2) + (curveCornerRadius * 3);
+    protected static final double boundaryRectangleBorderHeight = boundaryRectangleMinWidth;
+
+
+    protected static final Color zoneStandardColour = visibleWhite;
+    protected static final ColorName zoneSelectedColorName = ColorName.LIGHTGRAY;
+    protected static final Color zoneSelectedColor = zoneSelectedColorName.getColor().setA(1);
+    protected static final ColorName zoneShadedColorName = ColorName.DARKGRAY;
+    protected static final Color zoneShadedColor = zoneShadedColorName.getColor().setA(1);
+
+    protected static final double zoneCornerRadius = curveCornerRadius - curveBorderWidth;
+
+
+    protected static final ColorName arrowColourName = ColorName.BLACK;
+    protected static final Color arrowColour = arrowColourName.getColor();
+    protected static final ColorName arrowSelectedColourName = ColorName.RED;
+    protected static final Color arrowSelectedColour = arrowSelectedColourName.getColor();
+
+    protected static final double arrowLineWidth = 2;
+    protected static final int pointsInArrowLine = 4;
+
+
+    // seems easiest way to find what is under the mouse at any point is to keep a record in the mouse handlers
+    private static ConcreteSyntaxElement underMouse;
+
     private Boolean onScreenChanged;
     private Boolean abstractSyntaxUpToDate;
 
     private AbstractSet<ConcreteSyntaxElement> intersectsWith;
+    protected ConcreteBoundaryRectangle myBoundaryRectangle; // everything should have only one except arrows, which pick source
+            // can always get from an arrow any way with destination's boundary rectangle.
 
     // Maybe need to clean up the types around this.
     private DiagramElement abstractSyntaxRepresentation;
     protected Text labelText;
 
     private double xCoord, yCoord;
-    private double boundingBoxX, getBoundingBoxY;  // FIXME : the shape gets this for free in Lienzo
+    //private double boundingBoxX, boundingBoxY;  // FIXME : the shape gets this for free in Lienzo
 
-    private Color borderColour;
-    private Color fillColour;
+    private Color borderColour, borderSelectedColour;
+    private Color fillColour, fillSelectedColour;
 
     private String label;
     private double labelTextSize;
     private double labelX, labelY;  //relative to elements x,y ??
 
+
+    // should this be a node ??
     private Shape concreteRepresentation;
 
-    ConcreteSyntaxElement() {
-        setHasChangedOnScreen();
-        setAbstractSyntaxNOTUpToDate();
-        //setFillColour(new Color(255, 255, 255));
-        setBorderColour(new Color(0, 0, 0));
-    }
+//    ConcreteSyntaxElement() {
+//        setHasChangedOnScreen();
+//        setAbstractSyntaxNOTUpToDate();
+//        //setFillColour(new Color(255, 255, 255));
+//        //setBorderColour(new Color(0, 0, 0));  // FIXME : to constants
+//    }
 
-    ConcreteSyntaxElement(double x, double y) {
+    ConcreteSyntaxElement(double x, double y, ConcreteSyntaxElement_TYPES type) {
         xCoord = x;
         yCoord = y;
+        setType(type);
         setHasChangedOnScreen();
         setAbstractSyntaxNOTUpToDate();
         //setFillColour(new Color(255, 255, 255));
-        setBorderColour(new Color(0, 0, 0));
+        // it's the alpha value on the constructor that makes it invisible = 0
+        //setBorderColour(new Color(0, 0, 0));  // FIXME : to constants
+    }
+
+    private void setType(ConcreteSyntaxElement_TYPES newType) {
+        myType = newType;
+    }
+
+    public ConcreteSyntaxElement_TYPES getType() {
+        return myType;
+    }
+
+    public abstract void setBoundaryRectangle(ConcreteBoundaryRectangle rect);// {
+//        myBoundaryRectangle = rect;
+//        rect.addChild(this);
+//    }
+
+    public ConcreteBoundaryRectangle getBoundaryRectangle() {
+        return myBoundaryRectangle;
     }
 
     public boolean hasChangedOnScreen() {
@@ -98,6 +188,21 @@ public abstract class ConcreteSyntaxElement {
         return label;
     }
 
+    protected Color getBorderSelectedColour() {
+        return borderSelectedColour;
+    }
+
+    protected void setBorderSelectedColour(Color colour) {
+        borderSelectedColour = colour;
+    }
+
+    protected Color getFillSelectedColour() {
+        return fillSelectedColour;
+    }
+
+    protected void setFillSelectedColour(Color colour) {
+        fillSelectedColour = colour;
+    }
     protected Color getBorderColour() {
         return borderColour;
     }
@@ -114,6 +219,26 @@ public abstract class ConcreteSyntaxElement {
         fillColour = colour;
     }
 
+    protected void setIsUnderMouse() {
+        underMouse = this;
+    }
+
+    public static ConcreteSyntaxElement getElementUnderMouse() {
+        return underMouse;
+    }
+
+    protected void paintColoursOnConcreteRepresentation() {
+        concreteRepresentation.setStrokeColor(getBorderColour());
+        concreteRepresentation.setFillColor(getFillColour());
+    }
+
+    protected void paintSelectedColoursOnConcreteRepresentation() {
+        concreteRepresentation.setStrokeColor(getBorderSelectedColour());
+        concreteRepresentation.setFillColor(getFillSelectedColour());
+    }
+
+
+
     public Boolean hasLabel() {
         return !(label == null);
     }
@@ -126,15 +251,21 @@ public abstract class ConcreteSyntaxElement {
         return yCoord;
     }
 
+    public Point2D topLeft() {
+        return new Point2D(getX(), getY());
+    }
 
     public void setMyDiagram(AbstractDiagram diagram) {
         getAbstractSyntaxRepresentation().setParent(diagram);
     }
 
-
+    // only really used for zones, but easier to implement like this than investigating types at runtime
+    public void swapShading() {}
 
     public abstract void makeAbstractRepresentation();
     public abstract void makeConcreteRepresentation();
     public abstract void drawOnLayer(Layer layer);
+    public abstract void setAsSelected();
+    public abstract void deleteMe();
 
 }
--- AbstractSyntaxInitial/src/com/mySampleApplication/client/ConcreteZone.java
+++ AbstractSyntaxInitial/src/com/mySampleApplication/client/ConcreteZone.java
@@ -1,23 +1,260 @@
 package com.mySampleApplication.client;
 
-import com.ait.lienzo.client.core.shape.Layer;
+import com.ait.lienzo.client.core.event.NodeMouseEnterEvent;
+import com.ait.lienzo.client.core.event.NodeMouseEnterHandler;
+import com.ait.lienzo.client.core.event.NodeMouseExitEvent;
+import com.ait.lienzo.client.core.event.NodeMouseExitHandler;
+import com.ait.lienzo.client.core.shape.*;
+import com.ait.lienzo.client.core.types.Point2D;
+import com.ait.lienzo.shared.core.types.Color;
+
+import java.util.AbstractSet;
+import java.util.HashSet;
 
 /**
- * Created by Michael on 6/08/2015.
+ * This superclass represents the regular zone that is inside each curve.  The zones arising from intersections
+ * are dealt with in the subclass ConcreteIntersectionZone.
+ *
+ * Hence the zones here are the same shape as the curves, just a bit smaller to account for the border of the square.
+ * There are no borders on zones.
  */
-public class ConcreteZone extends ConcreteSyntaxElement {
+public class ConcreteZone extends ConcreteRectangularSyntaxElement {
+
+    // might want to store intersecting zones ... when I create one, use the background grid, test potential
+    // intersections and store.  When a spider is added, is it in me, if yes, test the intersecting zones too.
+    // a spider's dot can be in only one zone (but the spider could be joined by a line to another of itself, and
+    // thus each of those dots is in a different zone).
+
+
+    private AbstractSet<ConcreteCurve> curvesImIn;
+
+    private Boolean isShaded;
+    Integer drawingLevel;
+
+
+
+
+    ConcreteZone(double x, double y) {
+        // This is the actual x,y of the zone the offset from the enclosing curves has already been taken away
+        super(x, y, ConcreteSyntaxElement_TYPES.CONCRETEZONE);
+
+        setNOTShaded();
+        setBorderSelectedColour(zoneSelectedColor);
+        setFillSelectedColour(zoneSelectedColor);
+        setCornerRadius(zoneCornerRadius);
+        setBorderWidth(0);
+        drawingLevel = 0;
+        curvesImIn = new HashSet<ConcreteCurve>();
+        topLeftIsCircle = botLeftIsCircle = topRightIsCircle = botRightIsCircle = true;
+    }
+
+
+    public void swapShading() {
+        if(shaded()) {
+            setNOTShaded();
+        } else {
+            setShaded();
+        }
+    }
+
+    public void setShaded() {
+        isShaded = true;
+        setBorderColour(zoneShadedColor);
+        setFillColour(zoneShadedColor);
+    }
+
+    public void setNOTShaded() {
+        isShaded = false;
+        setBorderColour(zoneStandardColour);
+        setFillColour(zoneStandardColour);
+    }
+
+    public boolean shaded() {
+        return isShaded;
+    }
+
+    public Integer getLevel() {
+        return drawingLevel;
+    }
+
+    protected AbstractSet<ConcreteCurve> getCurves() {
+        return curvesImIn;
+    }
+
+    protected void addEnclosingCurve(ConcreteCurve curve) {
+        getCurves().add(curve);
+    }
+
+    protected void removeEnclosingCurve(ConcreteCurve curve) {
+        getCurves().remove(curve);
+    }
+
+    public void increaseLevel() {
+        getBoundaryRectangle().removeZone(getLevel(), this);
+        drawingLevel++;
+        getBoundaryRectangle().addZone(getLevel(), this);
+    }
+
+    public void decreaseLevel() {
+        getBoundaryRectangle().removeZone(getLevel(), this);
+        drawingLevel--;
+        getBoundaryRectangle().addZone(getLevel(), this);
+    }
+
+    public void setLevel(Integer newLevel) {
+        getBoundaryRectangle().removeZone(getLevel(), this);
+        drawingLevel = newLevel;
+        getBoundaryRectangle().addZone(getLevel(), this);
+    }
+
+    @Override
+    public void setBoundaryRectangle(ConcreteBoundaryRectangle rect) {
+        myBoundaryRectangle = rect;
+        rect.addZone(getLevel(), this);
+    }
+
+
     @Override
     public void makeAbstractRepresentation() {
 
     }
 
+
     @Override
     public void makeConcreteRepresentation() {
 
+        if(hasChangedOnScreen()) {
+            final Rectangle concreteZone = new Rectangle(getWidth(), getHeight(), getCornerRadius());
+            setConcreteRepresentation(concreteZone);
+            setupConcreteRepresentation();
+
+            concreteZone.addNodeMouseEnterHandler(new NodeMouseEnterHandler() {
+                @Override
+                public void onNodeMouseEnter(NodeMouseEnterEvent event) {
+                    paintSelectedColoursOnConcreteRepresentation();
+                    setIsUnderMouse();
+                    concreteZone.getLayer().draw();
+                }
+            });
+            concreteZone.addNodeMouseExitHandler(new NodeMouseExitHandler() {
+                @Override
+                public void onNodeMouseExit(NodeMouseExitEvent event) {
+                    paintColoursOnConcreteRepresentation();
+                    concreteZone.getLayer().batch();
+                }
+            });
+        }
     }
 
     @Override
     public void drawOnLayer(Layer layer) {
+        layer.add(getConcreteRepresentation());
+    }
 
+
+    public void deleteMe() {
+        getConcreteRepresentation().setListening(false);
+        getBoundaryRectangle().getCurveLayer().remove(getConcreteRepresentation());
+        getBoundaryRectangle().removeZone(getLevel(), this);
+    }
+
+
+    // ---------------------------------------------------------------------------------------
+    //                          Geometry
+    // ---------------------------------------------------------------------------------------
+
+
+    // FIXME ... some of this geometry code needs to be refactored into CONCRETERECTANGLESYNTAXELEMENT
+
+
+
+    public boolean intersectsZone(ConcreteZone other) {
+
+        // assumes no small curves constraint
+        if(completelyEncloses(other) || other.completelyEncloses(this)) {
+            return true;
+        }
+
+        // still true for zones???
+        if(containsPoint(other.topLeftCentre()) || other.containsPoint(topLeftCentre())
+                || containsPoint(other.bottomLeftCentre()) || other.containsPoint(bottomLeftCentre())
+                || containsPoint(other.bottomRightCentre()) || other.containsPoint(bottomRightCentre()))  {
+            return true;
+        }
+
+        // last case is if they slice right through each other
+        if(getX() < other.getX() && other.getX() < getX() + getWidth()
+                && other.getY() < getY() && getY() < other.getY() + other.getHeight()) {
+            return true;
+        }
+        if(other.getX() < getX()  && getX() < other.getX() + other.getWidth()
+                && getY() < other.getY() && other.getY() < getY() + getHeight()) {
+            return true;
+        }
+
+
+        return false;
+    }
+
+
+
+
+    // Computes the zone arising from the intersection.  Draws it and adds it where required.
+    //
+    // Assumes
+    // no small intersections constraint
+    // that the zones do actually intersect
+    public void computeIntersection(ConcreteZone other) {
+        Point2D topLeft, bottomRight;
+
+        // are any of the intersection corners half circles
+        Boolean leftTopIsCircle, leftBotIsCircle, rightTopIsCircle, rightBotIsCircle;
+
+
+        if(completelyEncloses(other)) {
+            other.increaseLevel();
+            return;     // no new zones to add
+        }
+
+        if(other.completelyEncloses(this)) {
+            increaseLevel();
+            return;
+        }
+
+        // figure out the corners ... call a zone to make itself
+        // Just going to do it corner by corner to calculate the extent of the intersection and work out what's
+        // round and what's square.
+
+        // these are points in the original curve.  To make them the top left of the zone, need to subtract the
+        // line widths.
+        topLeft = new Point2D(Math.max(getX(), other.getX()) + getBorderWidth(), Math.max(getY(), other.getY()) + getBorderWidth());
+        bottomRight = new Point2D(Math.min(getX() + getWidth(), other.getX() + other.getWidth()) - getBorderWidth(),
+                Math.min(getY() + getHeight(), other.getY() + other.getHeight()) - getBorderWidth());
+
+        // what's round and what's square?
+
+        leftTopIsCircle = (containsPoint(other.topLeftCentre()) && other.topLeftIsCircle)
+                || (other.containsPoint(topLeftCentre()) && topLeftIsCircle);
+        leftBotIsCircle = (containsPoint(other.bottomLeftCentre()) && other.botLeftIsCircle)
+                || (other.containsPoint(bottomLeftCentre()) && botLeftIsCircle);
+        rightTopIsCircle = (containsPoint(other.topRightCentre()) && other.topRightIsCircle)
+                || (other.containsPoint(topRightCentre()) && topRightIsCircle);
+        rightBotIsCircle = (containsPoint(other.bottomRightCentre()) && other.botRightIsCircle)
+                || (other.containsPoint(bottomRightCentre()) && botRightIsCircle);
+
+        ConcreteZone z = new ConcreteIntersectionZone(topLeft, bottomRight, leftTopIsCircle, leftBotIsCircle, rightTopIsCircle, rightBotIsCircle);
+
+        z.setBoundaryRectangle(getBoundaryRectangle());
+
+        z.setLevel(Math.max(getLevel(), other.getLevel()) + 1);
+
+        for(ConcreteCurve curve : getCurves()) {
+            curve.addEnclosedZone(z);
+        }
+        for(ConcreteCurve curve : other.getCurves()) {
+            curve.addEnclosedZone(z);
+        }
+
+        z.makeConcreteRepresentation();
     }
 }
